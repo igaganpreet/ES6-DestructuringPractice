eb6642cf4dd4611fe9be7bd9c5fe8588abb6f48f
Gagan Preet <gaganpreet.dev@gmail.com>
2021-05-10 22:54:58 +0530

    destructure array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [honda, tesla] = cars;
+console.log(honda);
+
+const {
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+const {
+  coloursByPopularity: [hondaTopColour, hondaSecondColour]
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour, teslaSecondColour]
+} = tesla;
 
 ReactDOM.render(
   <table>
